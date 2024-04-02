be431a992dfee507a9fe18757df289e5e655ac50
jairsjunior <jairsjunior@gmail.com>
2024-04-03 00:06:49 +0200

    Add basic navigation

--- cmd/uiState.go
+++ cmd/uiState.go
@@ -1,10 +1,12 @@
 package cmd
 
 type UIState struct {
-	Profile       string            `yaml:"profile"`
-	Resource      Resource          `yaml:"resource"`
-	Command       Command           `yaml:"command"`
-	SelectedItems map[string]string `yaml:"selectedItems"`
+	Profile           string            `yaml:"profile"`
+	Resource          Resource          `yaml:"resource"`
+	Command           Command           `yaml:"command"`
+	SelectedItems     map[string]string `yaml:"selectedItems"`
+	CommandBarVisible bool              `yaml:"commandBarVisible"`
+	Breadcrumbs       []string          `yaml:"breadcrumbs"`
 }
 
-var UiState UIState = UIState{SelectedItems: make(map[string]string)}
+var UiState UIState = UIState{SelectedItems: make(map[string]string), Breadcrumbs: []string{}}

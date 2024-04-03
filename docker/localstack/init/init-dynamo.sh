71f1b5c2caa9aded5ff0beb842f6e260a0b36801
MrJack <36191829+biagiopietro@users.noreply.github.com>
2024-04-03 17:40:47 +0200

    Small development improvements

--- docker/localstack/init/init-dynamo.sh
+++ docker/localstack/init/init-dynamo.sh
@@ -6,46 +6,10 @@ awslocal dynamodb create-table \
     --attribute-definitions AttributeName=id,AttributeType=S \
     --billing-mode PAY_PER_REQUEST
 
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo"}, "data": {"S":"data"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo1"}, "data": {"S":"data1"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo2"}, "data": {"S":"data2"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo3"}, "data": {"S":"data3"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo4"}, "data": {"S":"data4"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo5"}, "data": {"S":"data5"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo6"}, "data": {"S":"data6"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo7"}, "data": {"S":"data7"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo8"}, "data": {"S":"data8"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo9"}, "data": {"S":"data9"}}'
-
-awslocal dynamodb put-item \
-    --table-name global01 \
-    --item '{"id":{"S":"foo10"}, "data": {"S":"data10"}}'
+for i in {1..200};
+do
+  data=$(echo '{"id":{"S":"foo__ID__"},"data":{"S":"data__ID__"}}' | sed "s/__ID__/$i/g")
+  awslocal dynamodb put-item \
+      --table-name table_for_pagination \
+      --item ${data}
+done
